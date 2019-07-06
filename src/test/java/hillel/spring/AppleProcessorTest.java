13741d860fde3c4de107a2caeaef9fef28f07640
azlatov <anatoly.zlatov@gmail.com>
2019-07-06 10:01:41 +0300

    stream samples

--- src/test/java/hillel/spring/AppleProcessorTest.java
+++ src/test/java/hillel/spring/AppleProcessorTest.java
@@ -7,6 +7,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -102,7 +103,8 @@ public class AppleProcessorTest {
 
         long count =
                 colorToApples.entrySet().stream()
-                .peek(System.out::println)
+                .peek(a -> System.out.println("sdf" + a))
+                        .filter(a ->  true)
                 .count();
 
         System.out.println(count);
