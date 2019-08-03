bc70490f871f9192d1addd57fc6c7342b3ef448e
azlatov <anatoly.zlatov@gmail.com>
2019-08-03 13:10:18 +0300

    added dependencies

--- src/test/java/hillel/spring/doctor/controller/DoctorControllerTest.java
+++ src/test/java/hillel/spring/doctor/controller/DoctorControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,7 +23,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-@Transactional
+//@Transactional
+//@DirtiesContext
 public class DoctorControllerTest {
 
     @Autowired
@@ -33,7 +35,7 @@ public class DoctorControllerTest {
 
     @Before
     public void clean() {
-        //doctorRepository.deleteAll();
+        doctorRepository.deleteAll();
         //System.out.println("cleaned: " + doctorRepository.findAll().size());
     }
 
